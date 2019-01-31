d5f4f3701b973ee124bace60be223239204e4e19
sorensenNiels <sorensen.niels@gmail.com>
2019-01-31 19:54:38 +0100

    eslint and babel config

---
+++ babel.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable func-names */
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage'
+      }
+    ],
+    '@babel/preset-react',
+    '@babel/preset-flow'
+  ];
+
+  const plugins = [
+    'react-hot-loader/babel',
+    'react-loadable/babel',
+    'dynamic-import-node',
+    'lodash',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import'
+  ];
+
+  if (api.env('production')) {
+    plugins.push('transform-remove-console');
+    plugins.push('transform-react-remove-prop-types');
+  }
+
+  return {
+    presets,
+    plugins
+  };
+};
